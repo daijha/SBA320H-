e1f225bc502f68cc0f4a3a862c88cf404309ee36
daijha <daijha.rabalais@gmail.com>
2025-07-08 11:10:06 -0400

    added advice button with event listener and conditional rendering

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,27 +1,30 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";// may noe need use effect here 
 
+function Advice() {
+  const [adv, setAdv] = useState(null); // using null instead of empty string helps the logical and in the return.
 
-function Advice(){
-const [adv , setAdv]= useState(" ")
-useEffect(() => {
-    async function getAdvice() {
-        let url = 'https://api.adviceslip.com/advice'// returns random advice slip as a slip object
-        const response = await fetch(url)
-        const data = await response.json()
-        setAdv(data.slip.advice)
-     console.log("advice:", data.slip.advice)// logs advice to console
-    } 
-      getAdvice()
-       
-} , [])
+  async function getAdvice() {
+    let url = "https://api.adviceslip.com/advice"; // returns random advice slip as a slip object
+    const response = await fetch(url);
+    const data = await response.json();
+    setAdv(data.slip.advice);
+    console.log("advice:", data.slip.advice); // logs advice to console
+  }
+  //   useEffect(() => {
+  //     getAdvice();
+  //   }, []);// rendered on the mount and i don't want that ...
 
-return (
-  
-<div>
-    <p>
-here is your advice: {adv}
-    </p>
-</div>
-)
-} 
-export default Advice
+  return (
+    <div>
+      <button
+        onClick={() => {
+          getAdvice();
+        }}
+      >
+        Tell me something
+      </button>
+      {adv && <p>Here is what you should know: <br/> {adv}</p>}
+    </div>
+  );
+}
+export default Advice;
